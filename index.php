4f16b4f4ef97b0448a447a8152b64210356f8042
shamdi <hamdi.skander@gmail.com>
2020-09-23 20:34:40 +0100

    Update navbar links names

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php include_once '_header.php'; ?>
     <h1>Home page</h1>
     <ul>
-        <li><a href="#">Link 1</a></li>
-        <li><a href="#">Link 2</a></li>
-        <li><a href="#">Link 3</a></li>
+        <li><a href="#">Page 1</a></li>
+        <li><a href="#">Page 2</a></li>
+        <li><a href="#">Page 3</a></li>
     </ul>
 <?php include_once '_footer.php'; ?>
